fd3580e575ef3e92b8eb5177a3510093f767808a
AliciaWang55 <131703090+AliciaWang55@users.noreply.github.com>
2025-02-25 01:05:03 -0800

    Update LinearRegressionRunner.java

--- LinearRegressionRunner.java
+++ LinearRegressionRunner.java
@@ -2,11 +2,18 @@ public class LinearRegressionRunner {
     public static void main(String[] args) {
         // Ice cream sales example (x: temperature, y: sales)
         double[][] iceCreamSalesData = {
-            {30, 200}, 
-            {40, 250}, 
-            {50, 300}, 
-            {60, 350}, 
-            {70, 400}
+            {14.2, 215}, 
+            {16.4, 325}, 
+            {11.9, 185}, 
+            {15.2, 332}, 
+            {18.5, 406},
+            {22.1, 522},
+            {19.4, 412},
+            {25.1, 614},
+            {23.4, 544},
+            {18.1, 421},
+            {22.6, 445},
+            {17.2, 408}
         };
 
         // Calculate slope (m) and y-intercept (b)
